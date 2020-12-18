cbed17ceb696237b86d59691addab3171a2fc094
Matias Pizarro <pinkystylee98@gmail.com>
2020-12-18 00:59:26 -0300

    xd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
 <div class="flex-center position-ref full-height">
 
     <div class="content">
-        <img src="/img/54_envios.jpg" alt="pizza house logo">
+        <img src="https://static.vecteezy.com/system/resources/previews/000/628/936/non_2x/shipping-truck-icon-vector.jpg" alt="pizza house logo">
         <div class="title m-b-md">
             Centro de envios
         </div>
